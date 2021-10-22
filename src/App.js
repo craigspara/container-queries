efc276d2e01c1637a5f49d54a001c59228b8f05f
cspara <cspara@ebsco.com>
2021-10-22 12:46:41 -0400

    chore: add data for list cards

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import "./styles.css";
 import Container from "./container";
 import Card from "./card";
+import * as data from "./data.json";
+
+import "./styles.css";
 
 export default function App() {
+    const { items } = data;
   return (
     <div className="App">
       <main className="wrapper">
         <Container>
-          <Card />
-          <Card />
-          <Card />
+            {items?.map((item, index) => <Card item={item} key={index}/>)}
         </Container>
       </main>
     </div>
--- src/card.js
+++ src/card.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({item}) => {
+    const {title, subTitle, shortDescription, longDescription, buttonLabel} = item;
   return (
     <article className="card">
       <header className="card__header">
-        <h2 className="card__heading">Foo</h2>
+        <h2 className="card__title">{title}</h2>
+        <p className="card__sub-title">{subTitle}</p>
       </header>
+        <div className="description">{longDescription}</div>
+        <footer className="card__footer">
+            <button className="card__footer-button" type="text">{buttonLabel}</button>
+        </footer>
     </article>
   );
 };
